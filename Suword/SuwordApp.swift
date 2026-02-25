f5438fbb9e06543be4ebb7f3677471c5f557739b
GhadyOmar <90251825+GhadyOmar@users.noreply.github.com>
2026-02-26 00:58:46 +0300

    Initial Commit

---
+++ Suword/SuwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuwordApp.swift
+//  Suword
+//
+//  Created by Ghady Al Omar on 09/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct SuwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuwordTests/SuwordTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuwordTests.swift
+//  SuwordTests
+//
+//  Created by Ghady Al Omar on 09/09/1447 AH.
+//
+
+import Testing
+@testable import Suword
+
+struct SuwordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
